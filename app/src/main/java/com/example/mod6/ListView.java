4b9be44b8e70d499b7289be40f2d0b8d96f48c6c
BibitPamungkas321 <siy.laiylaiy69@gmail.com>
2020-05-13 11:21:14 +0700

    versil

---
+++ app/src/main/java/com/example/mod6/ListView.java
@@ -0,0 +1,11 @@
+package com.example.mod6;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListView extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_item);
+    }
+}
---
+++ app/src/main/java/com/example/mod6/konfigurasi.java
@@ -0,0 +1,25 @@
+package com.example.mod6;
+
+public class konfigurasi {
+    public static final String URL_ADD="http://192.168.1.16/db_android/new/tambah.php";
+    public static final String URL_GET_ALL = "http://192.168.1.16/db_android/new/lihat_data.php";
+    public static final String URL_GET_EMP = "http://192.168.1.16/db_android/new/lihat_data2?id=";
+    public static final String URL_UPDATE_EMP = "http://192.168.1.16/db_android/new/update.php";
+    public static final String URL_DELETE_EMP = "http://192.168.1.16/db_android/new/delete.php?id=";
+
+
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+
+
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+
+
+    public static final String MHS_ID = "mhs_id";
+}
